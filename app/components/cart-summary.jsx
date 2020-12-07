b5914cb73bbac35711bbc17f55c01b25003500bb
James Sangalli <j.l.sangalli@gmail.com>
2020-12-07 16:53:45 +1100

    refactor count in cart and wishlist

--- app/components/cart-summary.jsx
+++ app/components/cart-summary.jsx
@@ -14,14 +14,14 @@ class CartSummary extends Component {
             <h4>Shopping Cart</h4>
             <div className='products'>
                 {products.map((product, idx) => {
-                    const number = Math.max(this.props.cart.reduce((n, val) => { return n + (val === product.get("id")) }) - 1, 1);
+                    const number = itemCount(this.props.cart, product.get('id'));
                     return <div key={idx}>{ number + "x " + product.get('name') + " ($" + number * parseInt(product.get('price')) + ")" }</div>
                 })}
             </div>
             <h4>Wishlist</h4>
             <div className='wishlist'>
                 {wishlist.map((wish, idx) => {
-                    const number = Math.max(this.props.wishlist.reduce((n, val) => { return n + (val === wish.get("id")) }) - 1, 1);
+                    const number = itemCount(this.props.wishlist, wish.get('id'));
                     return <div key={idx}>{ number + "x " + wish.get('name') + " ($" + number * parseInt(wish.get('price')) + ")" }</div>
                 })}
             </div>
@@ -29,6 +29,12 @@ class CartSummary extends Component {
         </div>
     )
   }
+}
+
+function itemCount(items, id) {
+    return items.filter((item) => {
+        return item === id;
+    }).size;
 }
 
 function mapStateToProps(state) {
